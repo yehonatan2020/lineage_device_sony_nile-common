64bedfed197dd7d7d2df1ce38cdd19dc2648c4ce
yehonatan2020 <yehonatan@yandex.ru>
2024-02-01 20:55:48 +0000

    nile.mk: trim

--- nile.mk
+++ nile.mk
@@ -209,12 +209,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # For config.fs
 PRODUCT_PACKAGES += \
     fs_config_files
